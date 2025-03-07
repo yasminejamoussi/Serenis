37cbdc28aab8ac3c20c13320dea4a7e57f718181
yasmine <jamoussi.yasmine@esprit.tn>
2025-03-07 08:44:18 +0100

    integ

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+
       colors: {
         bleuhh: '#3164C4',     // Bleu
         orangehh: '#B14343',   // Orange
         verthh: '#4E7040',     // Vert
         fondhh: '#F5E6C4',     // Fond
         navfoohh : '#FFF3D9'
-
       },
       fontFamily: {
         sans: ['Roboto', 'sans-serif'],  // Police Roboto pour le texte
@@ -30,3 +30,4 @@ module.exports = {
   },
   plugins: [],
 }
+
